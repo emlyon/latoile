ae9668ed3b449b15dc9ffd8ff951cc79ffa759be
sjvl <samueljavelle@hotmail.com>
2021-04-01 14:17:18 +0200

    update

--- includes/footer.js
+++ includes/footer.js
@@ -7,15 +7,13 @@ include( `
 
                     <p class="grey-text text-lighten-4">
                         <ul>
-                            <li><a class="white-text" href="#">Conception web</a></li>
-                            <li><a class="white-text" href="#">Fabrication numérique</a></li>
-                            <li><a class="white-text" href="#">Objets connectés</a></li>
-                            <li><a class="white-text" href="#">Intelligence Artificielle</a></li>
+                            <li><a target="_blank" class="white-text" href="https://makerslab.em-lyon.com/fr/formations.html">Conception web</a></li>
+                            <li><a target="_blank" class="white-text" href="https://makerslab.em-lyon.com/fr/formations.html">Fabrication numérique</a></li>
+                            <li><a target="_blank" class="white-text" href="https://makerslab.em-lyon.com/fr/formations.html">Objets connectés</a></li>
+                            <li><a target="_blank" class="white-text" href="https://makerslab.em-lyon.com/fr/formations.html">Intelligence Artificielle</a></li>
                         </ul>
                     </p>
                 </div>
-
-                <div class="col l1 s12"></div>
 
                 <div class="col l2 s12">
                     <h5 class="white-text">Nos campus</h5>
@@ -31,22 +29,20 @@ include( `
 
                 <div class="col l1 s12"></div>
 
-                <div class="col l3 s12">
+                <div class="col l4 s12">
                     <h5 class="white-text">L'&#198;SCRA</h5>
 
                     <p class="grey-text text-lighten-4">
-                        L'&#198;SCRA est une association à but non lucratif régie par la loi du 1er juillet 1901. Elle a pour objectif de contribuer à la réduction des inégalités sociales ou culturelles et à la réalisation de l'égalité entre les hommes et les femmes en veillant à ce que tous ceux qui le souhaitent et le peuvent aient accès au développement de leurs compétences professionnelles.
+                        L'&#198;SCRA est une association à but non lucratif. Elle a pour objectif de contribuer à la réduction des inégalités sociales ou culturelles en développant des formations professionnelles.
                     </p>
                 </div>
 
-                <div class="col l1 s12"></div>
-
-                <div class="col l2 s12">
+                <div class="col l3 s12">
                     <h5 class="white-text">Nous contacter</h5>
 
                     <p class="grey-text text-lighten-4">
-                        Vous pouvez nous contacter par mail à l'adresse :</br>
-                        <a target="_blank" class="white-text" href="mailto:contact@aescra.com">contact@aescra.com</a></br>
+                        Vous pouvez nous contacter par mail :</br>
+                        <a target="_blank" class="white-text" href="mailto:contact@latoile.em-lyon.com">contact@latoile.em-lyon.com</a></br>
                         Ou par téléphone :</br>
                         <a target="_blank" class="white-text" href="tel:0478337800">04 78 33 78 00</a>
                     </p>
@@ -62,7 +58,8 @@ include( `
                         <a target="_blank" class="white-text" href="https://www.em-lyon.com/fr">Tous droits réservés &#169; emlyon business school</a> | <a class="white-text" href="#">mentions légales</a>
                     </p>
                 </div>
-                <div class="col l6 s12">
+                <div class="col l3 s12"></div>
+                <div class="col l3 s12">
                     <p>
                         <a target="_blank" class="white-text" href="https://twitter.com/makerslab_">Twitter</a> | <a target="_blank" class="white-text" href="https://www.instagram.com/makerslab_/">Instagram</a> | <a target="_blank" class="white-text" href="https://github.com/emlyon">Github</a>
                     </ul>
--- includes/navbar.js
+++ includes/navbar.js
@@ -2,16 +2,20 @@ include( `
     <div class="navbar-fixed">
         <nav class="white" role="navigation">
             <div class="nav-wrapper container">
-                <img class="move" src="./media/logo-emlyon.svg" alt="emlyon.svg"/>
-                <a id="logo-container" href="./" class="brand-logo"><img src="./media/logo.svg" alt="la toile.svg"/></a>
+                <a id="logo-container" href="./" class="brand-logo"><img src="./media/logo-latoile.svg" alt="la toile.svg"/></a>
                 <ul class="right hide-on-med-and-down">
+                    <li><a href="#mission">Notre mission</a></li>
                     <li><a href="#programme">Le programme</a></li>
-                    <li><a href="#candidatez">Candidatez</a></li>
+                    <li><a href="#candidatez">Candidater</a></li>
                     <li><a href="#footer">Contact</a></li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
+                    <li><a href="#mission">Notre mission</a></li>
+                    <li><a href="#programme">Le programme</a></li>
+                    <li><a href="#candidatez">Candidater</a></li>
+                    <li><a href="#footer">Contact</a></li>
                 </ul>
-                <a href="#" data-activates="nav-mobile"></a>
+                <a href="#" data-activates="nav-mobile" class="button-collapse"><i style="color: #e2001a;" class="material-icons">menu</i></a>
             </div>
         </nav>
     </div>
