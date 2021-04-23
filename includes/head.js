3533571baa7bf0cc1207ee4ec7a22b3407659c86
ashugeo <hugo.saintemarie@gmail.com>
2021-04-23 14:41:28 +0200

    Add Google Site Verification

--- includes/head.js
+++ includes/head.js
@@ -3,6 +3,9 @@ include( `
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>la toile</title>
 
+    <!-- Google Site Verification -->
+    <meta name="google-site-verification" content="ITWMbywFy9FgkD99pkvPZRbsQ1THAz5flpdjbh4D2ig" />
+
     <!-- CSS -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css" rel="stylesheet" media="screen,projection">
